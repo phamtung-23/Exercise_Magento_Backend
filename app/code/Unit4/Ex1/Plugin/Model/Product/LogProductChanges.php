a0c2556eac7eb3ac67e478292f499b1a931d74cc
phamtung-23 <ptung230801@gmail.com>
2023-06-13 18:33:44 +0700

    add plugin ex2 unit4

---
+++ app/code/Unit4/Ex1/Plugin/Model/Product/LogProductChanges.php
@@ -0,0 +1,42 @@
+<?php
+namespace Unit4\Ex1\Plugin\Model\Product;
+
+use Psr\Log\LoggerInterface;
+use \Magento\Catalog\Model\ResourceModel\Product;
+use \Magento\Framework\DataObject ;
+
+class LogProductChanges
+{
+    /**
+     * @var LoggerInterface
+     */
+    protected $logger;
+
+    /**
+     * LogPageOutput constructor.
+     * @param LoggerInterface $logger
+     */
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
+
+    public function beforeSave(
+        Product $subject,
+        DataObject $object
+    ) {
+        // @todo here need more complex function for generated data that contains old value and new value
+    //    $result = array_udiff_assoc($object->getData(), $object->getOrigData(),
+    //        function ($a,$b){
+    //            if (is_array($a) or is_array($b) or is_object($a) or is_object($b)) {
+    //                return false;
+    //            }
+
+    //            return $a !== $b;
+    //        }
+    //        );
+        
+        dd($object->getOrigData());
+        // $this->logger->debug(json_encode($object->getData()));
+    }
+}
